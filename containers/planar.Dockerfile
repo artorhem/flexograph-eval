a25688930721bc10779f8b76a2d4c9473bbad77f
Puneet Mehrotra <p.puneetmehrotra@outlook.com>
2024-09-30 01:33:43 -0700

    Planar compiles successfully

--- containers/planar.Dockerfile
+++ containers/planar.Dockerfile
@@ -6,7 +6,7 @@ LABEL description="Build environment for Planar"
 USER root
 SHELL [ "/bin/bash" , "-c" ]
 
-RUN apt-get update && apt-get --no-install-recommends -y install build-essential cmake git libboost-all-dev libomp-dev wget gdb time gnupg2 python3 vim
+RUN apt-get update && apt-get --no-install-recommends -y install build-essential cmake git libboost-all-dev libomp-dev wget gdb time gnupg2 python3 vim libssl-dev
 RUN apt-get install --reinstall -y  ca-certificates
 
 ADD scripts/planar.sh /planar.sh
--- scripts/planar.sh
+++ scripts/planar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PLANAR_DIR=/systems/ooc/planar
+PLANAR_DIR=/systems/ooc/Planar
 FOLLY_DIR=/folly
 FOLLY_BUILD_DIR=/folly_build
 
@@ -9,9 +9,12 @@ if [ "$PWD" != "/systems/ooc/planar" ]; then
     cd /systems/ooc/planar
 fi
 
+echo "Building Planar"
+echo "initializing submodules"
 git submodule update --init --recursive
 git submodule update
 
+echo "Building yaml-cpp (required by Planar)"
 #make yaml-cpp
 cd $PLANAR_DIR/third_party/yaml-cpp
 mkdir -p build
@@ -19,13 +22,32 @@ cd build
 cmake ..
 make -j
 
+
+echo "Building liburing (required by Planar)"
 #clone and build liburing
 cd $PLANAR_DIR/third_party
 git clone https://github.com/axboe/liburing.git
 cd liburing
 ./configure && make -j && make install
 
+echo "Building folly (required by Planar)"
 #Now clone and build folly
 cd / && git clone https://github.com/facebook/folly.git
 cd $FOLLY_DIR
 python3 ./build/fbcode_builder/getdeps.py --allow-system-packages --scratch-path=$FOLLY_BUILD_DIR build
+
+echo "Now building Planar"
+cd $PLANAR_DIR
+mkdir -p build; cd build
+
+PREFIX_PATH="$FOLLY_BUILD_DIR/installed/folly"
+#Now we need to find where libfmt is installed
+FMT_PATH=$(ls /folly_build/installed | grep fmt)
+#append fmt to the prefix path
+eval PREFIX_PATH="\"${PREFIX_PATH};${FOLLY_BUILD_DIR}/installed/${FMT_PATH}\""
+cmake -DCMAKE_PREFIX_PATH=${PREFIX_PATH} ..
+make -j
+
+echo "Planar built successfully"
+
+#Now we need to run the tests.
