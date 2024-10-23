31f07bc66e7b3d3aeb380fdb8d8d814e265c8455
Puneet Mehrotra <p.puneetmehrotra@outlook.com>
2024-10-23 02:12:01 -0700

    added checks to clone only if needed

--- get-systems.sh
+++ get-systems.sh
@@ -9,11 +9,15 @@ mkdir -p systems/in-mem
 cd systems/in-mem
 
 #Now clone the systems
-git clone git@github.com:artorhem/Galois.git #Galois
-git clone git@github.com:artorhem/gapbs.git #GAPBS
-git clone git@github.com:artorhem/ligra.git #Ligra
-git clone git@github.com:artorhem/GeminiGraph.git #GeminiGraph
-git clone git@github.com:artorhem/graphit.git #GraphIt
+systems=( "galois" "gapbs" "ligra" "gemini" "graphit" )
+for system in "${systems[@]}"
+do
+    if [ -d "$system" ]; then
+        echo "$system exists"
+    else
+        git clone git@github.com:artorhem/$system.git
+    fi
+done
 
 # Now add the out-of-core systems
 cd ../../
@@ -21,13 +25,24 @@ mkdir -p systems/ooc
 cd systems/ooc
 
 #Now clone the systems
-git clone git@github.com:artorhem/blaze.git #Blaze
-git clone git@github.com:artorhem/graphchi-cpp.git #GraphChi
-git clone git@github.com:artorhem/lumos.git #Lumos
-git clone git@github.com:SICS-Fundamental-Research-Center/Planar.git #Planar, VLDB2024
+systems=("blaze" "graphchi-cpp" "lumos" )
+for system in "${systems[@]}"
+do
+    if [ -d "$system" ]; then
+        echo "$system exists"
+    else
+        git clone git@github.com:artorhem/$system.git
+    fi
+done
+
+#git clone git@github.com:SICS-Fundamental-Research-Center/Planar.git #Planar, VLDB2024
 
 #Clone X-Stream and checkout their sosp branch
-git clone git@github.com:artorhem/x-stream.git 
+if [ -d "x-stream" ]; then
+  echo "X-Stream already cloned"
+else
+  git clone git@github.com:artorhem/x-stream.git 
+fi
 cd x-stream
 git checkout sosp
 
@@ -35,3 +50,4 @@ cd ../../
 mkdir -p systems/FlexoGraph
 git clone git@github.com:ubc-systopia/margraphita.git FlexoGraph/
 cd systems/FlexoGraph
+
