98af592a48d42bdb959dcf9f981c531034586403
Puneet Mehrotra <p.puneetmehrotra@outlook.com>
2025-03-09 12:22:19 -0700

    adding blaze

---
+++ scripts/blaze/blaze.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import re
+
+SRC_DIR = "/systems/ooc/blaze"
+BUILD_DIR = "/systems/ooc/blaze/build"
+DATASET_DIR = "/mnt/nvme/"
+RESULTS_DIR = "/results/blaze"
+TEMP_DIR = "/extra_space"
+
+REPEATS = 5
+PR_MAX_ITERS = 20
+
+def convert_galois_to_blaze(galois_file, blaze_file):
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
